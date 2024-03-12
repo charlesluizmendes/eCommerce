65f815a011f0f9456725224c6d40d58f25916910
Charles Mendes <charlesluizmendes@gmail.com>
2024-03-12 19:37:45 -0300

    Retornando ProductId no Basket

--- src/Basket/Basket.Application/ViewModels/ItemViewModel.cs
+++ src/Basket/Basket.Application/ViewModels/ItemViewModel.cs
@@ -7,5 +7,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
+        public int ProductId { get; set; }
     }
 }
